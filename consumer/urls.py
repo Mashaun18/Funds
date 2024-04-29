535f5eb6c0d26cbe42c22187a64479ba501833b3
Mashaun18 <animashaunoreoluwa@gmail.com>
2024-04-29 14:49:38 +0100

    fix modify url file

--- consumer/urls.py
+++ consumer/urls.py
@@ -2,6 +2,7 @@ from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     
     path('login/', auth_views.LoginView.as_view(), name='login'),
